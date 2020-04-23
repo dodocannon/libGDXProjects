444b404e34d877451f50be0ef3c626c58f2c1c5f
Kevin <kevin12kim27@gmail.com>
2020-04-23 06:56:29 -0700

    gotta start on detailfragment and anychart implementation -need the ability to remove portfolio stuff, and save the data.

---
+++ StickFigure/core/src/com/mygdx/game/TomatoWorld.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class TomatoWorld extends Stage {
+    public TomatoWorld(Viewport viewport, SpriteBatch spriteBatch)
+    {
+        super(viewport, spriteBatch);
+    }
+}
